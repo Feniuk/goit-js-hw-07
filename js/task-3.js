cd4b14fe0622be94bab4962b61c7a3d46a9ac8ed
Feniuk <143640539+Feniuk@users.noreply.github.com>
2023-12-30 16:18:10 +0100

    task 4 finish

--- js/task-3.js
+++ js/task-3.js
@@ -4,10 +4,11 @@ const outputText = document.querySelector('#name-output');
 inputText.addEventListener('input', onInputCongrats);
 
 function onInputCongrats(){
-    const inputValue = inputText.value.trim();
-    if (inputValue === '') {
-        nameOutput.textContent = 'Anonymous';
-      } else {
-        outputText.textContent = inputValue;
-      }
+  const inputValue = inputText.value.trim();
+  if (inputValue === '') { 
+      outputText.textContent = 'Anonymous'; 
+  } else {
+      outputText.textContent = inputValue;
+  }
 }
+    
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event){
+    event.preventDefault();
+    const emailValue = event.currentTarget.elements.email.value.trim();
+    const passwordValue = event.currentTarget.elements.password.value.trim();
+
+    if (emailValue === '' || passwordValue === '') {
+      alert('All form fields must be filled in');
+      return;
+    }
+    const email = loginForm.elements.email.value;
+    const password = loginForm.elements.password.value;
+    const formData = {
+      email,
+      password
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  };
